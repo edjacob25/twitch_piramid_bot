8eaec65dc0eee22d8644522e53e98ccbbbac82b2
Jacob Rivera <edjacob25@gmail.com>
2022-10-05 20:20:56 -0500

    Add hashtables to maintain state per channel, allow to connect to multiple channels

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rand::{
     Rng,
 };
 use serde::Deserialize;
+use std::collections::HashMap;
 use twitch_irc::login::StaticLoginCredentials;
 use twitch_irc::message::ServerMessage;
 use twitch_irc::ClientConfig;
@@ -73,52 +74,60 @@ pub async fn main() {
     let (mut incoming_messages, client) =
         TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(twitch_config);
 
+    let mut building_flags = HashMap::new();
+    let mut emote_counts = HashMap::new();
+    let mut emotes = HashMap::new();
+
+    for channel_to_connect in &conf.channels {
+        building_flags.insert(channel_to_connect.clone(), false);
+        emote_counts.insert(channel_to_connect.clone(), 0usize);
+        emotes.insert(channel_to_connect.clone(), "".to_owned());
+    }
+
     let cl = client.clone();
     let join_handle = tokio::spawn(async move {
-        let mut pyramid_building = false;
-        let mut emote_count = 0;
-        let mut emote = "".to_owned();
         while let Some(message) = incoming_messages.recv().await {
             match message {
                 ServerMessage::Privmsg(msg) => {
-                    println!(
-                        "(#{}) {}: {}",
-                        msg.channel_login, msg.sender.name, msg.message_text
-                    );
+                    let channel = msg.channel_login;
+                    let pyramid_building = building_flags.get_mut(channel.as_str()).unwrap();
+                    let emote_count = emote_counts.get_mut(channel.as_str()).unwrap();
+                    let emote = emotes.get_mut(channel.as_str()).unwrap();
+                    println!("(#{}) {}: {}", channel, msg.sender.name, msg.message_text);
                     if !msg.message_text.contains(" ") {
-                        pyramid_building = true;
-                        emote_count = 1;
-                        emote = msg.message_text;
-                        println!("Single word {}", emote);
-                    } else if pyramid_building {
+                        *pyramid_building = true;
+                        *emote_count = 1;
+                        *emote = msg.message_text;
+                        println!("Single word {}", *emote);
+                    } else if *pyramid_building {
                         let num_of_matches = msg
                             .message_text
-                            .match_indices(&emote)
+                            .match_indices(emote.as_str())
                             .collect::<Vec<_>>()
                             .len();
                         let num_of_words = msg.message_text.split(" ").collect::<Vec<_>>().len();
                         if num_of_words != num_of_matches {
-                            pyramid_building = false;
+                            *pyramid_building = false;
                             continue;
                         }
                         match num_of_matches {
-                            i if i == emote_count + 1 => {
+                            i if i == *emote_count + 1 => {
                                 println!("Pyramid growing");
-                                emote_count += 1;
+                                *emote_count += 1;
                             }
-                            i if i == emote_count - 1 => {
+                            i if i == *emote_count - 1 => {
                                 println!("Pyramid getting smaller");
-                                emote_count -= 1;
-                                if emote_count == 2 {
+                                *emote_count -= 1;
+                                if *emote_count == 2 {
                                     println!("Time to strike");
-                                    do_something(&cl, &msg.channel_login, &emote).await;
-                                    pyramid_building = false;
+                                    do_something(&cl, channel.as_str(), &emote).await;
+                                    *pyramid_building = false;
                                 }
                             }
-                            _ => pyramid_building = false,
+                            _ => *pyramid_building = false,
                         }
                     } else {
-                        pyramid_building = false;
+                        *pyramid_building = false;
                     }
                 }
                 _ => {
@@ -128,7 +137,9 @@ pub async fn main() {
         }
     });
 
-    client.join(conf.channels[0].clone()).unwrap();
+    for channel_to_connect in &conf.channels {
+        client.join(channel_to_connect.clone()).unwrap();
+    }
 
     // keep the tokio executor alive.
     // If you return instead of waiting the background task will exit.
